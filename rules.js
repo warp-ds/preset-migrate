cb6860626d4949745bd82eff41039db97e8e1726
BalbinaK <balbuhhha@gmail.com>
2023-02-24 15:46:18 +0100

    Add rules for deprecated fabric classes
    Co-authored-by: Anna Rybkina <anna.rybkina@anicode.se>

--- rules.js
+++ rules.js
@@ -1,14 +1,27 @@
+const TYPES = {
+  replaced: "[REPLACED]",
+  removed: "[REMOVED]"
+}
+
+const emitWarning = (selector, deprecationType, message) => {console.warn(`${deprecationType} ${selector}${message ? " -> " + message : ""}`)}
+
 export default [
-  [/bg-(blue|green)-(\d+)/, ([_, e]) => {
-    console.warn(`[REPLACED] ${_}`)
-  }],
-  [/aspect-([wh])/, ([_, e]) => {
-    console.warn(`[REPLACED] ${_} -> use e.g. aspect-1/1`)
-  }],
-  [/aspect-none/, ([_, e]) => {
-    console.warn(`[REMOVED] ${_}`)
-  }],
-  [/^focus-ring/, ([_, e]) => {
-    console.warn(`[REPLACED] ${_} -> use focusable`)
-  }]
+  [/^(text|bg|border|divide)-(blue|green|aqua|yellow|red|bluegray|gray)(-(\d+))?$/, ([_]) => emitWarning(_, TYPES.replaced)],
+  [/^(text|bg|border|divide)-(current|transparent|none|white)$/, ([_]) => emitWarning(_, TYPES.replaced)],
+  [/^text-(\d+)$/, ([_]) => emitWarning(_, TYPES.replaced)],
+  [/^aspect-([wh])$/, ([_]) => emitWarning(_, TYPES.replaced, "use e.g. aspect-1/1")],
+  [/^aspect-none$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^focus-ring$/, ([_]) =>  emitWarning(_, TYPES.replaced, "use focusable")],
+  [/^flex-(shrink|grow)(-0)?$/, ([_, sg, d]) =>  emitWarning(_, TYPES.replaced, `use ${sg}${d || ""}`)],
+  [/^(drop-)?shadow(-(2|3|4|10|20|30|40|none))?$/, ([_]) => emitWarning(_, TYPES.replaced)],
+  [/^decoration-(slice|none)/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^ring(-(.*))?$/, ([_]) => emitWarning(_, TYPES.removed)],
+  // filters
+  [/^(backdrop-)?(filter|filter-none)$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^(backdrop-)?blur(-(.*))?$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^(backdrop-)?(brightness|contrast|hue-rotate|saturate)-(\d+)$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^(backdrop-)?(invert|sepia)(-0)?$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^(backdrop-)?(invert|sepia)(-0)?$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^backdrop-opacity-(\d+)$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^outline-(none|white|black)$/, ([_]) => emitWarning(_, TYPES.removed)],
 ]
