dc699cc610031e7f566e41526cb896a1fcaf83ca
BalbinaK <balbuhhha@gmail.com>
2023-02-27 14:47:08 +0100

    Apply variations to the existing rules

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import rules from './rules.js'
+import {
+  variantBreakpoints,
+  variantImportant,
+  variantNegative,
+  variantTaggedPseudoClasses,
+  variantPseudoClassFunctions,
+  variantPseudoClassesAndElements,
+} from '@unocss/preset-mini/variants';
+
+const variants = [
+  variantBreakpoints(),
+  variantImportant(),
+  variantNegative,
+  variantPseudoClassFunctions(),
+  variantPseudoClassesAndElements(),
+  ...variantTaggedPseudoClasses({ attributifyPseudo: false }),
+];
 
 export const presetMigrate = () => {
   console.log("Read more about supported classes here: https://warp-ds.github.io/css-docs/")
 
-  return { name: '@warp-ds/preset-migrate', rules }
+  return { name: '@warp-ds/preset-migrate', rules , variants }
 } 
