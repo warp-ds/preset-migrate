789556c32cd9248012d17157357576ee6ca3e1b1
BalbinaK <balbuhhha@gmail.com>
2023-02-27 16:09:47 +0100

    Add remaining rules for text-, divide-, decoration- and component classes

--- rules.js
+++ rules.js
@@ -8,13 +8,15 @@ const emitWarning = (selector, deprecationType, message) => {console.warn(`${dep
 export default [
   [/^(text|bg|border|divide)-(blue|green|aqua|yellow|red|bluegray|gray)(-(\d+))?$/, ([_]) => emitWarning(_, TYPES.replaced)],
   [/^(text|bg|border|divide)-(current|transparent|none|white)$/, ([_]) => emitWarning(_, TYPES.replaced)],
+  [/^divide-(dotted|solid|double|dashed)$/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^text-(\d+)$/, ([_]) => emitWarning(_, TYPES.replaced)],
+  [/^text-(primary|secondary|danger)$/, ([_]) => emitWarning(_, TYPES.replaced)],
   [/^aspect-([wh])$/, ([_]) => emitWarning(_, TYPES.replaced, "use e.g. aspect-1/1")],
   [/^aspect-none$/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^focus-ring$/, ([_]) =>  emitWarning(_, TYPES.replaced, "use focusable")],
   [/^flex-(shrink|grow)(-0)?$/, ([_, sg, d]) =>  emitWarning(_, TYPES.replaced, `use ${sg}${d || ""}`)],
   [/^(drop-)?shadow(-(2|3|4|10|20|30|40|none))?$/, ([_]) => emitWarning(_, TYPES.replaced)],
-  [/^decoration-(slice|none)/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^decoration-(slice|none|clone)/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^ring(-(.*))?$/, ([_]) => emitWarning(_, TYPES.removed)],
   // filters
   [/^(backdrop-)?(filter|filter-none)$/, ([_]) => emitWarning(_, TYPES.removed)],
@@ -24,4 +26,6 @@ export default [
   [/^(backdrop-)?(invert|sepia)(-0)?$/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^backdrop-opacity-(\d+)$/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^outline-(none|white|black)$/, ([_]) => emitWarning(_, TYPES.removed)],
+  // component-specific
+  [/^(button|input|f)(-(.+))?$/, ([_]) => emitWarning(_, TYPES.removed, "use Warp components instead")],
 ]
