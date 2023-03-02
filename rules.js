3e5e3ec729911417c54d2b0474a0e29505969b81
BalbinaK <balbuhhha@gmail.com>
2023-03-02 09:58:34 +0100

    Add tests to colors.js

--- rules.js
+++ rules.js
@@ -1,12 +1,16 @@
-const TYPES = {
+export const TYPES = {
   replaced: "[REPLACED]",
   removed: "[REMOVED]"
 }
 
 const emitWarning = (selector, deprecationType, message) => {console.warn(`${deprecationType} ${selector}${message ? " -> " + message : ""}`)}
 
+export const colors = ["blue", "green", "aqua", "yellow", "red", "bluegray", "gray"]
+
+const colorRegex = new RegExp("^(text|bg|border|divide)-(" + colors.join("|") + ")(-(\\d+))?$")
+
 export default [
-  [/^(text|bg|border|divide)-(blue|green|aqua|yellow|red|bluegray|gray)(-(\d+))?$/, ([_]) => emitWarning(_, TYPES.replaced)],
+  [colorRegex, ([_]) => emitWarning(_, TYPES.replaced)],
   [/^(text|bg|border|divide)-(current|transparent|none|white)$/, ([_]) => emitWarning(_, TYPES.replaced)],
   [/^divide-(dotted|solid|double|dashed)$/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^text-(\d+)$/, ([_]) => emitWarning(_, TYPES.replaced)],
@@ -16,7 +20,7 @@ export default [
   [/^flex-(shrink|grow)(-0)?$/, ([_, sg, d]) =>  emitWarning(_, TYPES.replaced, `use ${sg}${d || ""}`)],
   [/^(drop-)?shadow(-(2|3|4|10|20|30|40|none))?$/, ([_]) => emitWarning(_, TYPES.replaced)],
   [/^decoration-(slice|none|clone)/, ([_]) => emitWarning(_, TYPES.removed)],
-  [/^ring(-(.*))?$/, ([_]) => emitWarning(_, TYPES.removed)],
+  [/^ring(?:-(.*))$/, ([_]) => emitWarning(_, TYPES.removed)],
   // filters
   [/^(backdrop-)?(filter|filter-none)$/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^(backdrop-)?blur(-(.*))?$/, ([_]) => emitWarning(_, TYPES.removed)],
@@ -32,7 +36,7 @@ export default [
   [/^t-(.+)$/, ([_]) => emitWarning(_, TYPES.removed)],
   [/^container$/, ([_]) => emitWarning(_, TYPES.removed)],
   // miscellaneous
-  [/^focus-ring$/, ([_]) =>  emitWarning(_, TYPES.replaced, "use focusable")],
+  [/^focus-ring$/,  ([_]) => emitWarning(_, TYPES.removed)],
   [/^transition-gpu$/, ([_]) => emitWarning(_, TYPES.removed, "use 'transform-gpu' or 'will-change-*'")],
   [/^fixed-ios-fix$/,([_]) => emitWarning(_, TYPES.removed, "use 'transform translate-z-0'")],
 ]
---
+++ test/color.spec.js
@@ -0,0 +1,89 @@
+import { setup } from "./_helpers.js";
+import { describe, expect, test, vi } from "vitest";
+import { colors, TYPES } from "../rules.js"
+
+setup();
+
+const colorHues = Array.from({ length: 10 }, (_, index) => index === 0 ? 50 : index * 100); // 50, 100, 200, ..., 900
+
+describe("colors", () => {
+  test("Emits a warning if text class is used with a color", async (t) => {
+    const warnSpy = vi.spyOn(global.console, 'warn')
+
+    const classes = colors.map((color) =>{
+      return colorHues.map(hue => `text-${color}-${hue}`)
+    }).flat()
+
+    const { css } = await t.uno.generate(classes);
+
+    expect(css).toMatchInlineSnapshot('""');
+    expect(warnSpy).toHaveBeenCalledTimes(classes.length);
+    classes.forEach((value) => {
+      expect(warnSpy).toHaveBeenCalledWith(`${TYPES.replaced} ${value}`)
+    })
+  });
+
+  test("Emits a warning if border class is used with a color", async (t) => {
+    const warnSpy = vi.spyOn(global.console, 'warn')
+
+    const classes = colors.map((color) =>{
+      return colorHues.map(hue => `border-${color}-${hue}`)
+    }).flat()
+
+    const { css } = await t.uno.generate(classes);
+
+    expect(css).toMatchInlineSnapshot('""');
+    expect(warnSpy).toHaveBeenCalledTimes(classes.length);
+    classes.forEach((value) => {
+      expect(warnSpy).toHaveBeenCalledWith(`${TYPES.replaced} ${value}`)
+    })
+  });
+
+  test("Emits a warning if background class is used with a color", async (t) => {
+    const warnSpy = vi.spyOn(global.console, 'warn')
+
+    const classes = colors.map((color) =>{
+      return colorHues.map(hue => `bg-${color}-${hue}`)
+    }).flat()
+
+    const { css } = await t.uno.generate(classes);
+
+    expect(css).toMatchInlineSnapshot('""');
+    expect(warnSpy).toHaveBeenCalledTimes(classes.length);
+    classes.forEach((value) => {
+      expect(warnSpy).toHaveBeenCalledWith(`${TYPES.replaced} ${value}`)
+    })
+  });
+
+  test("Emits a warning if divide class is used with a color", async (t) => {
+    const warnSpy = vi.spyOn(global.console, 'warn')
+
+    const classes = colors.map((color) =>{
+      return colorHues.map(hue => `divide-${color}-${hue}`)
+    }).flat()
+
+    const { css } = await t.uno.generate(classes);
+
+    expect(css).toMatchInlineSnapshot('""');
+    expect(warnSpy).toHaveBeenCalledTimes(classes.length);
+    classes.forEach((value) => {
+      expect(warnSpy).toHaveBeenCalledWith(`${TYPES.replaced} ${value}`)
+    })
+  });
+
+  test("Emits a warning if 'text', 'bg', 'border' or 'divide' is used with 'current', 'transparent', 'none' or 'white'", async (t) => {
+    const warnSpy = vi.spyOn(global.console, 'warn')
+
+    const classes = ["current", "transparent", "none", "white"].map((color) =>{
+      return ["text", "bg", "border", "divide"].map(el => `${el}-${color}`)
+    }).flat()
+
+    const { css } = await t.uno.generate(classes);
+
+    expect(css).toMatchInlineSnapshot('""');
+    expect(warnSpy).toHaveBeenCalledTimes(classes.length);
+    classes.forEach((value) => {
+      expect(warnSpy).toHaveBeenCalledWith(`${TYPES.replaced} ${value}`)
+    })
+  });
+})
