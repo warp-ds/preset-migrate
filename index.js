9c5f3e21a5c11b541e477990191d1bb6818a1cfa
Magnus Hübner <magnus.hubner@aptitud.se>
2024-07-02 09:20:46 +0200

    fix: Better intro message

--- index.js
+++ index.js
@@ -23,12 +23,10 @@ const line = (minus = 0) => '-'.repeat((process?.stdout?.columns ?? 72) - minus)
 
 export const presetMigrate = () => {
   console.group(`${CONSOLE_COLORS.bright}${CONSOLE_COLORS.pink}Warp CSS${CONSOLE_COLORS.reset} ${line(9)}`);
-  console.log(`This UnoCSS preset is here to help you migrate from previous versions of Warp DS as well as legacy design systems (e.g. Troika, Fabric).`);
-  console.log(`Read more about supported CSS classes in Warp Tech Docs: ${CSS_DOCS_URL}`);
-  console.log('Warp utilises a system of coloring border, text, background and icons using semantic color tokens.');
-  console.log('You will need to manually find the most suitable semantic class (e.g. s-bg-primary) to replace any classes which sets static colors (e.g. bg-blue-500).');
-  console.log(`You can find currently supported components in Warp Tech Docs: ${TECH_DOCS_URL}`);
-  console.log(`${CONSOLE_COLORS.warn}REMINDER:${CONSOLE_COLORS.reset} Don't forget to remove 'preset-migrate' from your UnoCSS setup once you are done migrating. ${CONSOLE_COLORS.warn}:)${CONSOLE_COLORS.reset}`);
+  console.log(`This UnoCSS preset is here to help you migrate from previous versions\nof Warp DS as well as legacy design systems (e.g. Troika, Fabric).`);
+  console.log('\nWarp utilises a system of coloring border, text, background and icons\nusing semantic color tokens. You will need to manually find the most\nsuitable semantic class (e.g. s-bg-primary) to replace any legacy\nclasses which sets static colors (e.g. bg-blue-500).');
+  console.log(`\nRead more about supported classes and components in Warp Tech Docs:\n${TECH_DOCS_URL}`);
+  console.log(`\n${CONSOLE_COLORS.warn}REMINDER:${CONSOLE_COLORS.reset} Don't forget to remove 'preset-migrate' from your UnoCSS\nsetup once you are done migrating. ${CONSOLE_COLORS.warn}:)${CONSOLE_COLORS.reset}`);
   console.groupEnd();
   console.log(line());
 
